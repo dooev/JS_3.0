e8e13ed5dfa5525ff5e989d451041a227489cc6a
Dima <dooevdima3@gmail.com>
2018-08-08 21:16:18 +0300

    lesson_1 (легкое задание) на for переделал

--- lesson_1/common.js
+++ lesson_1/common.js
@@ -10,18 +10,16 @@ mainList = {
 mainList.budget = budget;
 mainList.shopName = shopName;
 
-mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
-mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
-mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+// mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+// mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+// mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+
+for(let i = 0; i<3; i++) 
+	mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+alert(mainList.shopGoods);
 
 // console.log(mainList);
 
 var i = (mainList.budget / 30);
 
 alert("Бюджет на один день " + i + "руб");
-
-
-
-// for(let i = 0; i<3; i++) 
-// 	shopGoods.push(prompt("Какой тип товаров будем продавать?"));
-// alert(mainList.shopGoods);
