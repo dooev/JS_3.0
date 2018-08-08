95ed1eaff6c57032b351a2c97573e3f6f1000e1d
Dima <dooevdima3@gmail.com>
2018-08-08 18:20:35 +0300

    lesson_1 first commit (легкое задание)

--- lesson_1/common.js
+++ lesson_1/common.js
@@ -1,3 +1,27 @@
-console.log(4+4); 
-console.log(typeof(arr));
-console.log(typeof(arr));
+var shopName = prompt("Название Вашего магазина?");
+var budget = prompt("Ваш месячный бюджет?");
+
+mainList = {
+	shopGoods: [],
+	employers: {},
+	open: false
+};
+
+mainList.budget = budget;
+mainList.shopName = shopName;
+
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+
+// console.log(mainList);
+
+var i = (mainList.budget / 30);
+
+alert("Бюджет на один день " + i + "руб");
+
+
+
+// for(let i = 0; i<3; i++) 
+// 	shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+// alert(mainList.shopGoods);
