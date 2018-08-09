6b7858c2c3728982057f69cef2d625f6da32bff3
Dima <dooevdima3@gmail.com>
2018-08-09 19:00:26 +0300

    lesson_2_hard_1 Урок 2 сложное задание_1 выполнил первую часть

---
+++ lesson_2_hard_1/common.js
@@ -0,0 +1,52 @@
+let week= [
+	"Понедельник",
+	"Вторник",
+	"Среда",
+	"Четверг",
+	"Пятница",
+	"Суббота",
+	"Воскресенье"
+]; 
+for (var i = 0; i < week.length; i++) {
+
+	switch (week[i]) {
+		case "Понедельник":
+			document.write(week[i]+'<br>');
+			break;
+		case "Вторник":
+			document.write(week[i]+'<br>');
+			break;
+		case "Среда":
+			document.write(week[i]+'<br>');
+			break;
+		case "Четверг":
+			document.write('<i>'+week[i]+'</i>'+'<br>');
+			break;
+		case "Пятница":
+			document.write(week[i]+'<br>');
+			break;
+		case "Суббота":
+			document.write('<b>'+week[i]+'</b>'+'<br>');
+			break;
+		case "Воскресенье":
+			document.write('<b>'+week[i]+'</b>'+'<br>');
+			break;
+	}
+	
+}
+
+// Задание 2 START
+
+// arr =[
+// 	"22",
+// 	"132",
+// 	"3546",
+// 	"45657",
+// 	"798788",
+// 	"96795999",
+// 	"3356757999"
+// ];
+
+// for (var i = 0; i < arr.length; i++) {
+// 	if (arr[i].length[] == )
+// }
