ee3f4db086f079bfc276d6bb165c7c155bbf6eeb
Dima <dooevdima3@gmail.com>
2018-08-30 14:09:57 +0300

    Урок 9.2 правки присваивание контекста

--- lesson_9/lesson_9.2/common.js
+++ lesson_9/lesson_9.2/common.js
@@ -5,15 +5,5 @@ function showUser(surname, name) {
 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
 
-function User (name, surname){
-	this.name = name;
-	this.surname = surname;
-	this.age = age;
-}
-
-let Di = new User('Dima', 'Dooev'); 
-
-showUser.call(Di.age, Di.surname, Di.name);
-
-
-
+showUser.call(age,'Dima', 'Dooev');
+ // Если мы хотим передать в функцию что то в виде this, то пишим ИмяФункции.call(то что станет this, другие аргументы);  
